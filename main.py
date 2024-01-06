6df563309565891e1169caccd8f35b4dfeab15ce
Lienub <lienub.68@gmail.com>
2024-01-06 12:42:39 +0100

    chore(stablediffusion): modified main

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import pylla
 
 prompt = "A cute cat"
+img_url = "pylla/resources/image_bank/forest.jpeg"
 output_path = "image_cat.png"
 num_inference_steps = 5
 width = 256
 height = 256
 
-pylla.download_model()
-pylla.text_to_image(prompt, output_path, num_inference_steps, width, height)
+# pylla.download_model()
+# pylla.text_to_image(prompt, output_path, num_inference_steps, width, height)
+pylla.image_to_image(prompt, img_url, output_path, num_inference_steps, width, height)
