83d9b60faf1f9f1b8000ade649daa1f3b63c37fd
Damien WALTER <damien.walter@etu-unistra.fr>
2024-01-25 14:35:51 +0100

    fix: sudo verif

--- installation/installation.sh
+++ installation/installation.sh
@@ -2,10 +2,10 @@
 
 # Vérifier si le script est lancé avec sudo
 if [ "$EUID" -ne 0 ]; then
+    SUDO="sudo"
+else
     SUDO=""
     echo "Ce script peut nécessiter des droits sudo pour installer certains packages."
-else
-    SUDO="sudo"
 fi
 
 # Installer les modules nécessaires pour le machine learning avec Python
@@ -24,7 +24,10 @@ $SUDO pip3 install numpy pandas matplotlib scikit-learn tensorflow torch jupyter
 # Vérifier si apt-get est installé (pour Linux)
 if command -v apt-get >/dev/null 2>&1; then
     echo "Installation de python-tk..."
-    $SUDO apt-get install -y python-tk
+    if ! $SUDO apt-get install -y python-tk; then
+        echo "L'installation de python-tk a échoué. Veuillez vérifier les erreurs ci-dessus."
+        exit 1
+    fi
     echo "python-tk a été installé avec succès."
 else
     echo "apt-get n'est pas installé. Veuillez installer apt-get manuellement pour continuer l'installation de python-tk."
